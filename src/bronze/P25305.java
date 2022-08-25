241ecf00c8d01e1e08d2e246a08cd8c8771fad37
yeon228 <cumber88@naver.com>
2022-08-25 18:18:53 +0900

    solved p25305

--- src/bronze/P25305.java
+++ src/bronze/P25305.java
@@ -1,17 +1,18 @@
+package bronze;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Main{
+public class P25305 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-         int n = sc.nextInt();
-         int k = sc.nextInt();
-         ArrayList<Integer> arr = new ArrayList<>();
-         for(int i = 0; i < n; i ++){
-             arr.add(sc.nextInt());
-         }
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < n; i ++){
+            arr.add(sc.nextInt());
+        }
         Collections.sort(arr);
         System.out.println(arr.get(arr.size()-(k)));
     }
